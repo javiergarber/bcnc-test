1a14a693d001f06c1568c80d6a00360247537a27
Javi <javier_garber@yahoo.es>
2024-10-26 17:42:54 +0200

    Initial commit and setup

---
+++ src/main/java/com/bcnc/test/infrastructure/inbound/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bcnc.test.infrastructure.inbound;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
